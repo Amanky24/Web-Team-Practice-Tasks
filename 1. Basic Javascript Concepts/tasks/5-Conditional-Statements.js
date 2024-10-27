9a614289796e45fcc393b2d063009aba1e29fada
Amanky24 <aman8785vsecap@gmail.com>
2024-10-27 11:33:51 +0530

    Update 5-Conditional-Statements.js

--- 1. Basic Javascript Concepts/tasks/5-Conditional-Statements.js
+++ 1. Basic Javascript Concepts/tasks/5-Conditional-Statements.js
@@ -19,11 +19,16 @@ const accountSheet = [
 
 accountSheet.forEach(({ name, balance }) => {
 	// Edit the code over here
-	if (condition) {
+	if (balance>=50) {
 	// Do not edit the code until mentioned
 		console.log(name);
 	} 
 	// Continue editing the code after this
-
+	 else if(balance<15){
+        console.log(`$(name) is broke AF`);
+    	}
+    	else{
+        	console.log(balance);
+   	 }
 	// Do not edit the code beyond this point.
 });
